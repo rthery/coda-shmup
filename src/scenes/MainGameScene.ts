ead3a7a468ad8e517c303252aa904a43f29274bf
Romain THERY <romain.thery@ik.me>
2025-02-19 22:47:44 +0100

    Add player game object

--- src/scenes/MainGameScene.ts
+++ src/scenes/MainGameScene.ts
@@ -2,7 +2,8 @@ import { Scene, GameObjects } from 'phaser';
 
 export class MainGameScene extends Scene
 {
-    private textGO: GameObjects.Text;
+    private player: GameObjects.Triangle;
+    private playerCenter: GameObjects.Arc;
 
     constructor ()
     {
@@ -12,24 +13,21 @@ export class MainGameScene extends Scene
     preload ()
     {
         this.load.setPath('assets');
-        
-        this.load.image('background', 'bg.png');
-        this.load.image('logo', 'logo.png');
     }
 
     create ()
     {
-        this.add.image(512, 384, 'background');
-        this.add.image(512, 350, 'logo').setDepth(100);
-        this.textGO = this.add.text(512, 490, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
+        // https://coolors.co/palette/50514f-f25f5c-ffe066-247ba0-70c1b3
+        const colorPalette: string[] = ["#50514f","#f25f5c","#ffe066",
+            "#247ba0","#70c1b3"];
+        this.cameras.main.setBackgroundColor(colorPalette[0]);
+
+        this.player = this.add.triangle(this.cameras.main.centerX, this.cameras.main.height - 128, -1, 1, 1, 1, 0, -2, 0x247ba0).setScale(32).setDepth(100).setOrigin(0);
+        this.playerCenter = this.add.circle(this.player.x, this.player.y, 5, 0xf25f5c).setDepth(102);
     }
 
-    update (time: number, delta: number)
+    update (_time: number, _delta: number)
     {
-        this.textGO.text = `${time} ms\n${delta.toFixed(2)} ms`;
+
     }
 }
