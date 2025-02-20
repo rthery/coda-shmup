da409c7a45270d76b86dbbfec4bc9b0f058dc363
Romain THERY <romain.thery@ik.me>
2025-02-20 15:23:34 +0100

    Add enemies that player bullet can destroy

--- src/scenes/MainGameScene.ts
+++ src/scenes/MainGameScene.ts
@@ -7,6 +7,8 @@ export class MainGameScene extends Scene
     private playerRateOfFire: number = 0.5;
     private lastShotTime: number = 0;
     private cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
+    private bullets: Physics.Arcade.Group;
+    private enemies: Physics.Arcade.Group;
 
     constructor ()
     {
@@ -35,6 +37,39 @@ export class MainGameScene extends Scene
         {
             console.error('No keyboard input');
         }
+
+        // Add physics groups and callbacks
+        this.bullets = this.physics.add.group();
+        this.enemies = this.physics.add.group();
+        this.physics.add.collider(this.bullets, this.enemies, (bullet, enemy) => {
+            bullet.destroy();
+            enemy.destroy();
+        }, undefined, this);
+
+        // Spawn enemies indefinitely
+        this.time.addEvent({
+            delay: 1500,
+            callback: this.spawnEnemy,
+            callbackScope: this,
+            loop: true
+        });
+    }
+
+    private spawnEnemy ()
+    {
+        if (this.enemies.getLength() >= 5)
+        {
+            return;
+        }
+
+        const enemySize: number = 32;
+        let enemy: GameObjects.Arc = this.add.circle(Phaser.Math.Between(enemySize, this.cameras.main.width - enemySize), -enemySize/2, enemySize, 0xf25f5c).setDepth(100);
+        this.enemies.add(enemy);
+        this.physics.add.existing(enemy);
+        let enemyBody: Physics.Arcade.Body = enemy.body as Physics.Arcade.Body;
+        enemyBody.allowGravity = false;
+        enemyBody.setFriction(0, 0);
+        enemyBody.setVelocityY(256);
     }
 
     update (_timeSinceLaunch: number, deltaTime: number)
@@ -53,6 +88,7 @@ export class MainGameScene extends Scene
         if (this.cursorKeys.space.isDown && _timeSinceLaunch - this.lastShotTime > this.playerRateOfFire * 1000)
         {
             let bullet: GameObjects.Rectangle = this.add.rectangle(this.player.x, this.player.y - this.player.displayHeight / 2, 4, 12, 0xffe066).setOrigin(0.5);
+            this.bullets.add(bullet);
             this.physics.add.existing(bullet);
             let bulletBody: Physics.Arcade.Body = bullet.body as Physics.Arcade.Body;
             bulletBody.allowGravity = false;
@@ -64,5 +100,19 @@ export class MainGameScene extends Scene
 
         // Stop player from going offscreen
         this.player.x = Phaser.Math.Clamp(this.player.x, this.player.displayWidth / 2, this.cameras.main.width - this.player.displayWidth / 2);
+
+        // Destroy entities when out of screen
+        this.bullets.getChildren().forEach(bullet => {
+            if ((bullet as GameObjects.Rectangle).y < -(bullet as GameObjects.Rectangle).displayHeight)
+            {
+                bullet.destroy();
+            }
+        });
+        this.enemies.getChildren().forEach(enemy => {
+            if ((enemy as GameObjects.Arc).y >= this.cameras.main.height + (enemy as GameObjects.Arc).displayHeight)
+            {
+                enemy.destroy();
+            }
+        });
     }
 }
