8d0175447e7a466d84268a74bb1dc6745937443e
Romain THERY <romain.thery@ik.me>
2025-02-25 21:44:34 +0100

    Set default background color to black to ease transition between scenes

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ const config: Types.Core.GameConfig = {
     width: 1080,
     height: 1920,
     parent: 'game-container',
-    backgroundColor: '#028af8',
+    backgroundColor: '#000',
     scale: {
         mode: Scale.FIT,
         autoCenter: Scale.CENTER_BOTH
